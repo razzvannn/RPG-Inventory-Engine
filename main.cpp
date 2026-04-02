4a2fd3b3dfe8d3db4a601d484f5a385d7dcdc5d2
razzvannn <razvanstefanblanaru1010@gmail.com>
2026-04-02 20:10:26 +0300

    fix attempt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include "Meniu.h"
-
+#include "ItemSpecial.h"
 int main() {
     Meniu joc;
     joc.run();
+    ItemSpecial* testItem = new ItemSpecial("Test");
+    Item* copie = testItem->clona();
+    delete copie;
+    delete testItem;
     return 0;
 }
